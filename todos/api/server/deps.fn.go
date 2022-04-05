9075c13a72a253d97d9bdc6906da67fb251fedd1
Niklas Gehlen <niklas@namespacelabs.com>
2022-04-05 15:23:46 +0200

    Use generated postgres creds.

--- todos/api/server/deps.fn.go
+++ todos/api/server/deps.fn.go
@@ -13,8 +13,8 @@ import (
 	"namespacelabs.dev/foundation/std/go/grpc/server"
 	"namespacelabs.dev/foundation/std/monitoring/tracing"
 	"namespacelabs.dev/foundation/std/secrets"
-	"namespacelabs.dev/foundation/universe/db/postgres/creds"
 	"namespacelabs.dev/foundation/universe/db/postgres/incluster"
+	"namespacelabs.dev/foundation/universe/db/postgres/incluster/creds"
 )
 
 type ServerDeps struct {
@@ -57,30 +57,14 @@ func PrepareDeps(ctx context.Context) (*ServerDeps, error) {
 		PackageName: "namespacelabs.dev/foundation/std/secrets",
 		Instance:    "creds0",
 		Do: func(ctx context.Context) (err error) {
-			// name: "postgres_password_file"
-			// provision: PROVISION_INLINE
-			// provision: PROVISION_AS_FILE
+			// name: "postgres-password-file"
+			// generate: {
+			//   random_byte_count: 32
+			// }
 			p := &secrets.Secret{}
-			core.MustUnwrapProto("ChZwb3N0Z3Jlc19wYXNzd29yZF9maWxlEgIBAg==", p)
+			core.MustUnwrapProto("ChZwb3N0Z3Jlcy1wYXNzd29yZC1maWxlGgIQIA==", p)
 
-			if creds0.Password, err = secrets.ProvideSecret(ctx, "namespacelabs.dev/foundation/universe/db/postgres/creds", p); err != nil {
-				return err
-			}
-			return nil
-		},
-	})
-
-	di.Register(core.Initializer{
-		PackageName: "namespacelabs.dev/foundation/std/secrets",
-		Instance:    "creds0",
-		Do: func(ctx context.Context) (err error) {
-			// name: "postgres_user_file"
-			// provision: PROVISION_INLINE
-			// provision: PROVISION_AS_FILE
-			p := &secrets.Secret{}
-			core.MustUnwrapProto("ChJwb3N0Z3Jlc191c2VyX2ZpbGUSAgEC", p)
-
-			if creds0.User, err = secrets.ProvideSecret(ctx, "namespacelabs.dev/foundation/universe/db/postgres/creds", p); err != nil {
+			if creds0.Password, err = secrets.ProvideSecret(ctx, "namespacelabs.dev/foundation/universe/db/postgres/incluster/creds", p); err != nil {
 				return err
 			}
 			return nil
@@ -90,7 +74,7 @@ func PrepareDeps(ctx context.Context) (*ServerDeps, error) {
 	var incluster0 incluster.ExtensionDeps
 
 	di.Register(core.Initializer{
-		PackageName: "namespacelabs.dev/foundation/universe/db/postgres/creds",
+		PackageName: "namespacelabs.dev/foundation/universe/db/postgres/incluster/creds",
 		Instance:    "incluster0",
 		DependsOn:   []string{"creds0"}, Do: func(ctx context.Context) (err error) {
 			if incluster0.Creds, err = creds.ProvideCreds(ctx, "namespacelabs.dev/foundation/universe/db/postgres/incluster", nil, creds0); err != nil {
--- todos/go.mod
+++ todos/go.mod
@@ -7,11 +7,12 @@ require (
 	github.com/jackc/pgx/v4 v4.15.0
 	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.27.1
-	namespacelabs.dev/foundation v0.0.9
+	namespacelabs.dev/foundation v0.0.26-0.20220405131105-483394b4c97f
 	namespacelabs.dev/go-ids v0.0.0-20220216144417-79021816502e
 )
 
 require (
+	cuelang.org/go v0.4.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/go-logr/logr v1.2.2 // indirect
@@ -30,8 +31,11 @@ require (
 	github.com/jackc/pgtype v1.10.0 // indirect
 	github.com/jackc/puddle v1.2.1 // indirect
 	github.com/jxskiss/base62 v1.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/mpvl/unique v0.0.0-20150818121801-cbe035fff7de // indirect
 	github.com/prometheus/client_golang v1.12.1 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
@@ -44,6 +48,7 @@ require (
 	go.opentelemetry.io/otel/sdk v1.4.1 // indirect
 	go.opentelemetry.io/otel/trace v1.4.1 // indirect
 	golang.org/x/crypto v0.0.0-20211215165025-cf75a172585e // indirect
+	golang.org/x/exp v0.0.0-20220328175248-053ad81199eb // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
 	golang.org/x/text v0.3.7 // indirect
