2a27b06dd6b5993999ac621e5e566f97cea6cac5
Niklas Gehlen <niklas@namespacelabs.com>
2022-10-11 17:39:07 +0200

    Shell test: check HTTP status

--- multitier/01-simple/apibackend/test/test.sh
+++ multitier/01-simple/apibackend/test/test.sh
@@ -5,18 +5,23 @@ ENDPOINT=`cat /namespace/config/runtime.json | jq -r ".stack_entry[0].service[0]
 
 for NAME in item1 item2
 do
-    RESPONSE=`curl -s -X POST --data '{"name": "'$NAME'"}' $ENDPOINT/add`
-    if [[ ! -z "$RESPONSE" ]]; then
-        echo "failed to add $NAME: $RESPONSE"
+    STATUS=`curl -s -X POST -w '%{http_code}' -o response.txt --data '{"name": "'$NAME'"}' $ENDPOINT/add`
+    if [[ $STATUS -ne 200 ]]; then
+        echo "failed to add $NAME (status $STATUS)"
+        cat response.txt
         exit 1
     fi
 done
 
-RESPONSE=`curl -s $ENDPOINT/list`
-echo "Got list response:\n$RESPONSE"
+STATUS=`curl -s -w '%{http_code}' -o response.txt $ENDPOINT/list`
+if [[ $STATUS -ne 200 ]]; then
+    echo "failed to list items (status $STATUS)"
+    cat response.txt
+    exit 1
+fi
 
 # "-c" compacts the results to a single line.
-LIST=`echo $RESPONSE | jq -c 'map(.name)'`
+LIST=`cat response.txt | jq -c 'map(.name)'`
 EXPECTED="[\"item1\",\"item2\"]"
 if [[ "$LIST" != "$EXPECTED" ]]; then
     echo "Unexpected result: $LIST"
