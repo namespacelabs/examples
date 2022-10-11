6f8c333b89fc0014a7da7aa79b15b682ae8c44a4
Niklas Gehlen <niklas@namespacelabs.com>
2022-10-11 18:16:20 +0200

    Wire binary scaffolding.

--- golang/02-withresources/minio/prepare/main.go
+++ golang/02-withresources/minio/prepare/main.go
@@ -4,6 +4,43 @@
 
 package main
 
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+
+	"namespacelabs.dev/examples/golang/02-withresources/s3"
+)
+
+var intent = flag.String("intent", "", "The serialized JSON intent.")
+
 func main() {
-	// TODO add minio server to stack
+	_ = flag.String("resources", "", "The serialized JSON resources.")
+	flag.Parse()
+
+	if *intent == "" {
+		log.Fatal("--intent is missing")
+	}
+
+	i := &s3.S3Intent{}
+	if err := json.Unmarshal([]byte(*intent), i); err != nil {
+		log.Fatal(err)
+	}
+
+	// TODO generate keys
+	out := &s3.S3Instance{
+		Region:          i.Region,
+		AccessKey:       "TestOnlyUser",
+		SecretAccessKey: "TestOnlyPassword",
+	}
+
+	// TODO add minio to stack
+
+	serialized, err := json.Marshal(out)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("namespace.provision.result: %s\n", serialized)
 }
