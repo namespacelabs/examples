cf095de09c37d700707d16a4976e3fb5dafdabb6
Niklas Gehlen <niklas@namespacelabs.com>
2022-10-19 15:57:45 +0200

    code cleanups

--- golang/03-withresources/minio/prepare/main.go
+++ golang/03-withresources/minio/prepare/main.go
@@ -16,6 +16,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	awss3 "github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/cenkalti/backoff/v4"
@@ -40,108 +41,38 @@ func main() {
 
 	ctx := context.Background()
 
-	if *intent == "" {
-		log.Fatal("--intent is missing")
-	}
-
-	i := &s3.BucketIntent{}
-	if err := json.Unmarshal([]byte(*intent), i); err != nil {
-		log.Fatal(err)
-	}
-
-	if *resources == "" {
-		log.Fatal("--resources is missing")
-	}
-	r := fnresources.NewParser([]byte(*resources))
-
-	endpoint, err := getEndpoint(r)
+	instance, err := createInstance()
 	if err != nil {
 		log.Fatal(err)
 	}
-	url := fmt.Sprintf("http://%s", endpoint)
 
 	resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-		return aws.Endpoint{PartitionID: "aws", URL: url, SigningRegion: region}, nil
+		return aws.Endpoint{PartitionID: "aws", URL: instance.Url, SigningRegion: region}, nil
 	})
 
-	accessKeyID, err := readSecret(r, "minioUser")
-	if err != nil {
-		log.Fatal(err)
-	}
-	secretAccessKey, err := readSecret(r, "minioPassword")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	cfg, err := config.LoadDefaultConfig(ctx,
-		config.WithRegion(i.Region),
+		config.WithRegion(instance.Region),
 		config.WithEndpointResolverWithOptions(resolver),
 		config.WithCredentialsProvider(
-			credProvider{accessKeyID: accessKeyID, secretAccessKey: secretAccessKey}))
+			credentials.NewStaticCredentialsProvider(instance.AccessKey, instance.SecretAccessKey, "" /* session */)))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	cli := awss3.NewFromConfig(cfg, func(o *awss3.Options) {
 		o.UsePathStyle = true
 	})
 
-	// Retry until bucket is ready.
-	log.Printf("Creating bucket %s.\n", i.BucketName)
-	if err := backoff.Retry(func() error {
-		// Speed up bucket creation through faster retries.
-		ctx, cancel := context.WithTimeout(ctx, connBackoff)
-		defer cancel()
-
-		_, err := cli.CreateBucket(ctx, &awss3.CreateBucketInput{
-			Bucket: aws.String(i.BucketName),
-		})
-		var alreadyExists *types.BucketAlreadyExists
-		var alreadyOwned *types.BucketAlreadyOwnedByYou
-		if err == nil || errors.As(err, &alreadyExists) || errors.As(err, &alreadyOwned) {
-			return nil
-		}
-
-		// TODO remove when we set the right secrets!
-		if err != nil {
-			panic(err)
-		}
-
-		log.Printf("failed to create bucket: %v\n", err)
-		return err
-	}, backoff.WithContext(backoff.NewConstantBackOff(connBackoff), ctx)); err != nil {
-		panic(err)
-	}
-	log.Printf("Bucket %s created\n", i.BucketName)
-
-	out := &s3.BucketInstance{
-		Region:          i.Region,
-		AccessKey:       accessKeyID,
-		SecretAccessKey: secretAccessKey,
-		BucketName:      i.BucketName,
-		Url:             url,
+	if err := createBucket(ctx, cli, instance.BucketName); err != nil {
+		log.Fatal(err)
 	}
 
-	serialized, err := json.Marshal(out)
+	serialized, err := json.Marshal(instance)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Printf("namespace.provision.result: %s\n", serialized)
-}
-
-type credProvider struct {
-	accessKeyID     string
-	secretAccessKey string
-}
-
-var _ aws.CredentialsProvider = credProvider{}
-
-func (cf credProvider) Retrieve(ctx context.Context) (aws.Credentials, error) {
-	return aws.Credentials{
-		AccessKeyID:     cf.accessKeyID,
-		SecretAccessKey: cf.secretAccessKey,
-	}, nil
 }
 
 func getEndpoint(resources *fnresources.Parser) (string, error) {
@@ -173,4 +104,69 @@ func readSecret(resources *fnresources.Parser, name string) (string, error) {
 	}
 
 	return string(data), nil
+}
+
+func createInstance() (*s3.BucketInstance, error) {
+	if *intent == "" {
+		return nil, fmt.Errorf("--intent is missing")
+	}
+
+	i := &s3.BucketIntent{}
+	if err := json.Unmarshal([]byte(*intent), i); err != nil {
+		return nil, err
+	}
+
+	if *resources == "" {
+		return nil, fmt.Errorf("--resources is missing")
+	}
+	r := fnresources.NewParser([]byte(*resources))
+
+	endpoint, err := getEndpoint(r)
+	if err != nil {
+		return nil, err
+	}
+
+	accessKeyID, err := readSecret(r, "minioUser")
+	if err != nil {
+		return nil, err
+	}
+	secretAccessKey, err := readSecret(r, "minioPassword")
+	if err != nil {
+		return nil, err
+	}
+
+	return &s3.BucketInstance{
+		Region:          i.Region,
+		AccessKey:       accessKeyID,
+		SecretAccessKey: secretAccessKey,
+		BucketName:      i.BucketName,
+		Url:             fmt.Sprintf("http://%s", endpoint),
+	}, nil
+}
+
+func createBucket(ctx context.Context, cli *awss3.Client, bucketName string) error {
+	// Retry until bucket is ready.
+	log.Printf("Creating bucket %s.\n", bucketName)
+	if err := backoff.Retry(func() error {
+		// Speed up bucket creation through faster retries.
+		ctx, cancel := context.WithTimeout(ctx, connBackoff)
+		defer cancel()
+
+		_, err := cli.CreateBucket(ctx, &awss3.CreateBucketInput{
+			Bucket: aws.String(bucketName),
+		})
+		var alreadyExists *types.BucketAlreadyExists
+		var alreadyOwned *types.BucketAlreadyOwnedByYou
+		if err == nil || errors.As(err, &alreadyExists) || errors.As(err, &alreadyOwned) {
+			return nil
+		}
+
+		log.Printf("failed to create bucket: %v\n", err)
+		return err
+	}, backoff.WithContext(backoff.NewConstantBackOff(connBackoff), ctx)); err != nil {
+		return err
+	}
+	log.Printf("Bucket %s created\n", bucketName)
+
+	return nil
 }
--- golang/03-withresources/server/main.go
+++ golang/03-withresources/server/main.go
@@ -12,9 +12,9 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	awss3 "github.com/aws/aws-sdk-go-v2/service/s3"
 	"namespacelabs.dev/examples/golang/03-withresources/s3"
-	fnresources "namespacelabs.dev/foundation/framework/resources"
 	"namespacelabs.dev/foundation/std/go/core"
 )
 
@@ -32,15 +32,18 @@ func main() {
 		panic(err)
 	}
 
-	cli, err := connectS3(ctx, resources)
+	bucket := &s3.BucketInstance{}
+	if err := resources.Decode(minioResource, bucket); err != nil {
+		panic(err)
+	}
+
+	cli, err := connectS3(ctx, bucket)
 	if err != nil {
 		panic(err)
 	}
 
-	bucketname := "TODO from resource"
-
-	http.HandleFunc("/put", put(ctx, cli, bucketname))
-	http.HandleFunc("/get", get(ctx, cli, bucketname))
+	http.HandleFunc("/put", put(ctx, cli, bucket.BucketName))
+	http.HandleFunc("/get", get(ctx, cli, bucket.BucketName))
 	http.HandleFunc("/readyz", func(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(200)
 		fmt.Fprintf(rw, "All OK\n\n")
@@ -51,12 +54,7 @@ func main() {
 	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
 
-func connectS3(ctx context.Context, resources *fnresources.Parser) (*awss3.Client, error) {
-	var bucket s3.BucketInstance
-
-	if err := resources.Decode(minioResource, &bucket); err != nil {
-		return nil, err
-	}
+func connectS3(ctx context.Context, bucket *s3.BucketInstance) (*awss3.Client, error) {
 
 	resolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
 		return aws.Endpoint{PartitionID: "aws", URL: bucket.Url, SigningRegion: region}, nil
@@ -66,7 +64,7 @@ func connectS3(ctx context.Context, resources *fnresources.Parser) (*awss3.Clien
 		config.WithRegion(bucket.Region),
 		config.WithEndpointResolverWithOptions(resolver),
 		config.WithCredentialsProvider(
-			credProvider{accessKeyID: bucket.AccessKey, secretAccessKey: bucket.SecretAccessKey}))
+			credentials.NewStaticCredentialsProvider(bucket.AccessKey, bucket.SecretAccessKey, "" /* session */)))
 	if err != nil {
 		return nil, err
 	}
@@ -74,18 +72,4 @@ func connectS3(ctx context.Context, resources *fnresources.Parser) (*awss3.Clien
 	return awss3.NewFromConfig(cfg, func(o *awss3.Options) {
 		o.UsePathStyle = true
 	}), nil
-}
-
-type credProvider struct {
-	accessKeyID     string
-	secretAccessKey string
-}
-
-var _ aws.CredentialsProvider = credProvider{}
-
-func (cf credProvider) Retrieve(ctx context.Context) (aws.Credentials, error) {
-	return aws.Credentials{
-		AccessKeyID:     cf.accessKeyID,
-		SecretAccessKey: cf.secretAccessKey,
-	}, nil
 }
