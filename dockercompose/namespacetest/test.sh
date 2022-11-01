24942f4c2fac007bb9078b7493f1052735388001
Niklas Gehlen <niklas@namespacelabs.com>
2022-11-01 13:57:35 +0100

    Simplify all curl tests.

--- dockercompose/namespacetest/test.sh
+++ dockercompose/namespacetest/test.sh
@@ -3,12 +3,11 @@ set -e
 
 # "-r" removes quotes from the output.
 ENDPOINT=`cat /namespace/config/runtime.json | jq -r ".stack_entry[0].service[0].endpoint"`
-
 echo "Extracted endpoint $ENDPOINT"
 
-STATUS=`curl -X POST -w '%{http_code}' -o response.txt --data '{"name": "'testItem'"}' $ENDPOINT/add`
+STATUS=`curl -X GET -w '%{http_code}' $ENDPOINT`
 if [[ $STATUS -ne 200 ]]; then
-    echo "failed to add item (status $STATUS)"
+    echo "failed to curl $ENDPOINT (status $STATUS)"
     exit 1
 fi
 
