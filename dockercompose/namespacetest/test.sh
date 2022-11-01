0ea55250cdc6a144312cf6cbf1b4a3f51413627d
Niklas Gehlen <niklas@namespacelabs.com>
2022-11-01 14:22:20 +0100

    Clean up curl test output.

--- dockercompose/namespacetest/test.sh
+++ dockercompose/namespacetest/test.sh
@@ -5,10 +5,10 @@ set -e
 ENDPOINT=`cat /namespace/config/runtime.json | jq -r ".stack_entry[0].service[0].endpoint"`
 echo "Extracted endpoint $ENDPOINT"
 
-STATUS=`curl -X GET -w '%{http_code}' $ENDPOINT`
+STATUS=`curl -X GET -w '%{http_code}' -o response.txt $ENDPOINT`
 if [[ $STATUS -ne 200 ]]; then
     echo "failed to curl $ENDPOINT (status $STATUS)"
     exit 1
 fi
 
-echo "SUCCESS!"
+cat response.txt
