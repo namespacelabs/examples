5f28eedbc5b85c30c4cbb314c8390319202e8545
Niklas Gehlen <niklas@namespacelabs.com>
2022-11-02 15:02:38 +0100

    Curl tests: Tidy up output

--- multitier/02-withsecrets/apibackend/tests/curl.sh
+++ multitier/02-withsecrets/apibackend/tests/curl.sh
@@ -3,12 +3,10 @@ set -e
 
 # "-r" removes quotes from the output.
 ENDPOINT=`cat /namespace/config/runtime.json | jq -r ".stack_entry[0].service[0].endpoint"`
-echo "Extracted endpoint $ENDPOINT"
 
-STATUS=`curl -X GET -w '%{http_code}' -o response.txt $ENDPOINT`
+STATUS=`curl -X GET -w '%{http_code}' -o response.txt -s $ENDPOINT`
+
+echo "curl $ENDPOINT returned status $STATUS"
 if [[ $STATUS -ne 200 ]]; then
-    echo "failed to curl $ENDPOINT (status $STATUS)"
     exit 1
 fi
-
-cat response.txt
