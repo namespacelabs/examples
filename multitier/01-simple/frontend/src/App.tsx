ff4062f19923bbbd422fcb325321e59b655dfb69
Nik Zherebtsov <nz@namespacelabs.com>
2022-11-22 17:33:20 +0100

    Beautifying "multitier" examples.

--- multitier/01-simple/frontend/src/App.tsx
+++ multitier/01-simple/frontend/src/App.tsx
@@ -3,17 +3,33 @@
 // you may not use this file except in compliance with the License.
 
 import { useQuery, useQueryClient } from "react-query";
-import styles from "./App.module.css";
+import { Chrome } from "./Chrome";
 import { todosService } from "./lib/todoservice";
 
 function App() {
 	const queryClient = useQueryClient();
 
 	return (
-		<div>
-			<main className={styles.main}>
+		<Chrome
+			description={
+				<>
+					<div>
+						In this example Namespace deployed a multitier application that consists of:
+						<ul className="list-disc list-inside px-5">
+							<li>A Vite frontend</li>
+							<li>An API server written in Go</li>
+							<li>A Postgres instance for the persistence layer</li>
+						</ul>
+					</div>
+					<p className="py-5">
+						When you add an item, the api server will handle your request, and store the data in
+						Postgres.
+					</p>
+				</>
+			}>
+			<div className="flex flex-col bg-white text-black p-5">
 				<form
-					className={styles.form}
+					className="flex"
 					onSubmit={async (e) => {
 						e.preventDefault();
 
@@ -22,12 +38,23 @@ function App() {
 						nameInput.value = "";
 						queryClient.invalidateQueries("todoList");
 					}}>
-					<input type="text" name="name" />
-					<button type="submit">Add</button>
+					<input
+						type="text"
+						name="name"
+						className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+						placeholder="Type the item description"
+					/>
+					<div className="mt-5 sm:mt-0 sm:ml-6 sm:flex sm:flex-shrink-0 sm:items-center">
+						<button
+							type="submit"
+							className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm">
+							Add item
+						</button>
+					</div>
 				</form>
 				<TodoList />
-			</main>
-		</div>
+			</div>
+		</Chrome>
 	);
 }
 
@@ -43,11 +70,43 @@ function TodoList() {
 	}
 
 	return (
-		<ul>
-			{data?.map((todo) => (
-				<li key={todo.id}>{todo.name}</li>
-			))}
-		</ul>
+		<div className="py-4">
+			<div className="mt-8 flex flex-col">
+				<div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+					<div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+						<div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+							<table className="min-w-full divide-y divide-gray-300">
+								<thead className="bg-gray-50">
+									<tr>
+										<th
+											scope="col"
+											className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+											Items
+										</th>
+									</tr>
+								</thead>
+								<tbody className="divide-y divide-gray-200 bg-white">
+									{data?.map((todo) => (
+										<tr key={todo.id}>
+											<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-normal text-gray-900 sm:pl-6">
+												{todo.name}
+											</td>
+										</tr>
+									))}
+									{!data && (
+										<tr>
+											<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-normal text-gray-500 sm:pl-6">
+												No items yet
+											</td>
+										</tr>
+									)}
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
---
+++ multitier/03-withresources/frontend/src/Chrome.tsx
@@ -0,0 +1,51 @@
+// Copyright 2022 Namespace Labs Inc; All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+
+import { ReactNode } from "react";
+import logo from "./assets/logo.svg";
+
+export function Chrome(props: { description: ReactNode; children: ReactNode }) {
+	return (
+		<div>
+			<div className="flex flex-col absolute top-0 left-0 right-0 bottom-0 justify-center items-center">
+				<div>
+					<div className="mb-8 flex">
+						<img src={logo} className="w-12 h-12" />
+					</div>
+
+					<div className="mb-24">
+						<div className="mx-auto max-w-2xl text-left">
+							<h2 className="text-xl font-bold tracking-tight text-white sm:text-4xl">
+								<span className="block">Welcome to Namespace.</span>
+							</h2>
+							<div className="mt-4 text-lg leading-6 text-indigo-200">{props.description}</div>
+							<a
+								href="https://namespace.so/docs"
+								target="_blank"
+								className="inline-flex items-center justify-center text-base font-medium text-indigo-600">
+								Documentation
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									strokeWidth="1.5"
+									stroke="currentColor"
+									className="w-5 h-5 ml-2">
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+									/>
+								</svg>
+							</a>
+						</div>
+					</div>
+					<div className="mx-auto max-w-2xl text-left rounded-xl border border-indigo-200 overflow-hidden">
+						{props.children}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
